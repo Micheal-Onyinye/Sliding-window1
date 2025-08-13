add0bf8b1ccd48cb09ee419a1f879bdfab95b79a
Micheal Onyinyechi <michealphilominaprecious@gmail.com>
2025-08-13 13:10:34 +0100

    Rename files and update functions

--- netflix_buffer_run.py
+++
@@ -1,9 +0,0 @@
-from netflix_buffer import netflix_buffer
-
-# Example usage
-
-video_chunks = [1, 2, 3, 4, 5] # movie chunks
-
-window_size = 2 # can hold 2 chunks at once
-
-netflix_buffer(video_chunks, window_size)
